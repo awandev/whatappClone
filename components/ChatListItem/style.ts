a53ee2f6e34a1efa3afe40f352bc4e3267eb37e0
awandev <awal.kurniawan@uin-alauddin.ac.id>
2021-01-01 10:11:39 +0800

    finish chatroomlist component

--- components/ChatListItem/style.ts
+++ components/ChatListItem/style.ts
@@ -1,10 +1,40 @@
 import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
-    avatar: {
-        width: 50,
-        height: 50,
 
+    container: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        padding: 10,
+    },
+
+    leftContainer: {
+        flexDirection: 'row',
+    },
+    midContainer: {
+        justifyContent: 'space-around',
+    },
+
+    avatar: {
+        width: 60,
+        height: 60,
+        marginHorizontal: 10,
+        marginRight: 15,
+        borderRadius: 25,
+    },
+
+    username: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+    lastMessage: {
+        fontSize: 16,
+        color: 'grey',
+    },
+    time: {
+        fontSize: 16,
+        color: 'grey',
     }
 });
 
